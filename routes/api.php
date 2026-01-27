c865b902d21b1e8f26e683d4828f0089275cd2a8
Serious-Sam97 <samzuckerberg.jar@gmail.com>
2026-01-26 22:15:41 -0300

    Cors fixed | sanctum | api and web setup

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
 Route::get('/debug-session', function () {
     return session()->all();
 });
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware(['auth:sanctum'])->get('/api/user', function (Request $request) {
+    return $request->user();
 });
